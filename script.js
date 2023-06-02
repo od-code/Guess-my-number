8253dcba4d58b98d2283ea443529281ce7510338
Derrick Atongo <derrickatongo@gmail.com>
2023-06-02 08:58:58 +0300

    Finished implementing the game logic

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 "use strict";
+// The random generated number
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
-// document.querySelector(".number").textContent = randomNumber;
+document.querySelector(".number").textContent = randomNumber;
 console.log(randomNumber);
-let scoreValue = document.querySelector(".score").textContent;
-
+// Store the values of the scores in variables
 let score = 20;
 let highscore = 0;
 
-// Handling the click event for the check button
 document.querySelector(".check").addEventListener("click", function () {
+  // Grab the number from the input
   const number = Number(document.querySelector(".guess").value);
-  console.log(number);
+  // If there is no guess
   if (!number) {
     document.querySelector(".message").textContent = "⛔No number";
-    // The guess is correct
+    // If the guess is correct
   } else if (number === randomNumber) {
-    document.querySelector(".message").textContent = "🎉Correct Number";
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".highscore").textContent = score;
-    // The guess is incorrect
+    document.querySelector(".message").textContent = "🎉🎉Correct Number";
+    document.querySelector(".glow").textContent = "🙌🎊You Won🎊🙌";
+    document.querySelector("body").style.backgroundColor = "#3E4B87";
+    if (score > highscore) {
+      document.querySelector(".highscore").textContent = score;
+    }
   } else if (number !== randomNumber && score > 0) {
-    // The guess is low
-    if (number < randomNumber) {
-      document.querySelector(".message").textContent = "📉Too Low!!";
+    // If the guess is high
+    if (number > randomNumber) {
+      document.querySelector(".message").textContent = "📈Too High!";
       score--;
       document.querySelector(".score").textContent = score;
-
-      // the guess is high
-    } else if (number > randomNumber) {
-      document.querySelector(".message").textContent = "📈Too High!!";
+      // If the guess is low
+    } else if (number < randomNumber) {
+      document.querySelector(".message").textContent = "📉Too Low";
       score--;
       document.querySelector(".score").textContent = score;
     }
-    if (document.querySelector(".score").textContent == 0) {
-      document.querySelector(".message").textContent = "😔You've lost the game";
+    // The score is zero
+    if (score === 0) {
+      document.querySelector(".message").textContent =
+        "😔😔You Have Lost The game";
     }
   }
+});
+// Resetting the game
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#000";
+  document.querySelector(".glow").textContent = "Guess My Number!";
+  document.querySelector(".score").textContent = 20;
 });
