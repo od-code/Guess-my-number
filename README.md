197e3f2b39983da51a592e35042225c71084d51a
Derrick Atongo <derrickatongo@gmail.com>
2023-06-04 11:20:28 +0300

    Added readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Number Guessing game
+
+- This is a project for Jonas Smedtmans' JavaScript course which requires the player to guess a number between 1 and 20 which will be compared to the randomly generated number created when the game.
--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 "use strict";
 // The random generated number
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = randomNumber;
-console.log(randomNumber);
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 // Store the values of the scores in variables
 let score = 20;
 let highscore = 0;
@@ -16,34 +14,34 @@ document.querySelector(".check").addEventListener("click", function () {
     // If the guess is correct
   } else if (number === randomNumber) {
     document.querySelector(".message").textContent = "🎉🎉Correct Number";
+    document.querySelector(".number").textContent = randomNumber;
     document.querySelector(".glow").textContent = "🙌🎊You Won🎊🙌";
     document.querySelector("body").style.backgroundColor = "#3E4B87";
-    if (score > highscore) {
-      document.querySelector(".highscore").textContent = score;
-    }
+    score > highscore ? (highscore = score) : highscore;
+    document.querySelector(".highscore").textContent = highscore;
   } else if (number !== randomNumber && score > 0) {
-    // If the guess is high
-    if (number > randomNumber) {
-      document.querySelector(".message").textContent = "📈Too High!";
-      score--;
-      document.querySelector(".score").textContent = score;
-      // If the guess is low
-    } else if (number < randomNumber) {
-      document.querySelector(".message").textContent = "📉Too Low";
-      score--;
-      document.querySelector(".score").textContent = score;
-    }
+    score--;
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".message").textContent =
+      number > randomNumber ? "📈Too High!" : "📉Too Low";
     // The score is zero
     if (score === 0) {
       document.querySelector(".message").textContent =
         "😔😔You Have Lost The game";
+      document.querySelector("body").style.backgroundColor = "#A9081C";
+      document.querySelector(".glow").textContent = "😔😔You Lost😔😔";
     }
   }
 });
 // Resetting the game
 document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(randomNumber);
   document.querySelector(".message").textContent = "Start guessing...";
   document.querySelector("body").style.backgroundColor = "#000";
   document.querySelector(".glow").textContent = "Guess My Number!";
-  document.querySelector(".score").textContent = 20;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = score;
 });
